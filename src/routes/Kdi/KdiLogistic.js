f139df8619fdc0c1849ca85aecc072602c684b69
jjl <329926193@qq.com>
2018-07-17 18:14:26 +0800

    Auto stash before merge of "master" and "origin/master"

--- src/routes/Kdi/KdiLogistic.js
+++ src/routes/Kdi/KdiLogistic.js
@@ -1,21 +1,69 @@
 import SimpleMng from 'components/Rebue/SimpleMng';
 import React from 'react';
 import { connect } from 'dva';
-import { Card, Table } from 'antd';
+import { Row, Col, Card, Form, Input, Select, Button, Table, DatePicker, Option } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import SysForm from './SysForm';
 import styles from './SysMng.less';
 
+const FormItem = Form.Item;
+const { RangePicker } = DatePicker;
 @connect(({ kdilogistic, loading }) => ({ kdilogistic, loading: loading.models.kdilogistic }))
+@Form.create()
 export default class KdiLogistic extends SimpleMng {
   constructor() {
     super();
     this.moduleCode = 'kdilogistic';
   }
+
+  renderSearchForm() {
+    console.info(this.props);
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 6, lg: 24, xl: 48 }}>
+          <Col md={6} sm={24}>
+            <FormItem label="">{getFieldDecorator('no')(<Input placeholder="收件人姓名/电话/快递单号" />)}</FormItem>
+          </Col>
+          <Col md={5} sm={24}>
+            <FormItem label="">
+              {getFieldDecorator('status')(
+                <Select placeholder="状态" style={{ width: '100%' }}>
+                  {/* <Option value="0">全部</Option> */}
+                  <Option value="2">无轨迹</Option>
+                  <Option value="3">已揽收</Option>
+                  <Option value="4">在途中</Option>
+                  <Option value="5">已签收</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col md={7} sm={24}>
+            <FormItem label="">
+              {getFieldDecorator('date')(
+                <RangePicker style={{ width: '100%' }} placeholder={['开始日期', '结束日期']} />
+              )}
+            </FormItem>
+          </Col>
+          <Col md={6} sm={24}>
+            <span style={{ float: 'left', marginBottom: 24 }}>
+              <Button type="primary" htmlType="submit">
+                查询
+              </Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+                重置
+              </Button>
+            </span>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
   render() {
     const { kdilogistic: { kdilogistic }, loading } = this.props;
     const { editForm, editFormType, editFormTitle, editFormRecord } = this.state;
-    console.info(kdilogistic);
     const columns = [
       {
         title: '订单号',
@@ -50,6 +98,7 @@ export default class KdiLogistic extends SimpleMng {
     return (
       <PageHeaderLayout title="快递订单管理">
         <Card bordered={false}>
+          <div className={styles.tableListForm}>{this.renderSearchForm()}</div>
           <div className={styles.tableList}>
             <Table rowKey="id" pagination={false} loading={loading} dataSource={kdilogistic} columns={columns} />
           </div>
