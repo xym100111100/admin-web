0306d7d920f4cf74ff75df9f920951f997687add
xym <253274870@qq.com>
2018-08-09 14:23:34 +0800

    修改了访问后台的路径

--- src/services/kdientry.js
+++ src/services/kdientry.js
@@ -1,7 +1,7 @@
 import request from '../utils/request';
 
 export async function add(params) {
-  return request('/kdi-svr/kdi/logistic', {
+  return request('/kdi-svr/kdi/logistic/entry', {
     method: 'POST',
     body: {
       ...params,
