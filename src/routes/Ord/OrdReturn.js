bd7d5c6b21f5d9a09166553aaada7a7b9920de74
xym <253274870@qq.com>
2018-10-27 17:15:13 +0800

    修改判断条件

--- src/routes/Ord/OrdReturn.js
+++ src/routes/Ord/OrdReturn.js
@@ -216,21 +216,21 @@ export default class OrdReturn extends SimpleMng {
                 {record.picList[0] !== undefined && (
                     <Col md={8} sm={24}>
                         <Popover content={content1} placement={'left'} >
-                            <div style={{ cursor: 'hand', cursor: 'pointer', width: 20, height: 20, backgroundImage: '/ise-svr/files' + record.picList[0].picPath }} ></div>
+                            <div style={{ cursor: 'hand', cursor: 'pointer', width: 20, height: 20, backgroundImage:'url(/ise-svr/files' + record.picList[0].picPath +')'}} ></div>
                         </Popover>
                     </Col>
                 )}
                 {record.picList[1] !== undefined && (
                     <Col md={8} sm={24}>
                         <Popover content={content2} placement={'left'} >
-                            <div style={{ cursor: 'hand', cursor: 'pointer', width: 20, height: 20, backgroundImage: '/ise-svr/files' + record.picList[1].picPath }} ></div>
+                            <div style={{ cursor: 'hand', cursor: 'pointer', width: 20, height: 20, backgroundImage: 'url(/ise-svr/files' + record.picList[1].picPath +')' }} ></div>
                         </Popover>
                     </Col>
                 )}
                 {record.picList[2] !== undefined && (
                     <Popover content={content3} placement={'left'} >
                         <Col md={8} sm={24}>
-                            <div style={{ cursor: 'hand', cursor: 'pointer', width: 20, height: 20, backgroundImage: '/ise-svr/files' + record.picList[2].picPath }} ></div>
+                            <div style={{ cursor: 'hand', cursor: 'pointer', width: 20, height: 20, backgroundImage: 'url(/ise-svr/files' + record.picList[2].picPath +')' }} ></div>
                         </Col>
                     </Popover>
                 )}
