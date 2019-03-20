e23563bf622cf87662877e1bedc3ed0a15a996c8
xym <253274870@qq.com>
2019-03-20 15:16:53 +0800

    修改区域信息

--- mock/xyzarea.js
+++ mock/xyzarea.js
@@ -2,10 +2,10 @@ import { parse } from 'url';
 
 // mock tableListDataSource
 const tableListDataSource = [
-  { id: 'pfm-admin', name: '平台后台管理', remark: '平台后台管理系统' },
-  { id: 'kdi-admin', name: '快递管理', remark: '快递管理系统' },
-  { id: 'damai-admin', name: '大卖网后台管理', remark: '大卖网后台管理系统' },
-  { id: 'wbl-admin', name: '微薄利后台管理', remark: '微薄利后台管理系统' },
+  { id: 'pfm-admin', areaName: '平台后台管理', remark: '平台后台管理系统',entryTime:'1995-05-06' ,},
+  { id: 'kdi-admin', areaName: '快递管理', remark: '快递管理系统' ,entryTime:'1995-05-06' },
+  { id: 'damai-admin', areaName: '大卖网后台管理', remark: '大卖网后台管理系统' ,entryTime:'1995-05-06' },
+  { id: 'wbl-admin', areaName: '微薄利后台管理', remark: '微薄利后台管理系统',entryTime:'1995-05-06'  },
 ];
 
 export function xyzareaList(req, res) {
